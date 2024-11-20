cac1591b57856b1a80b8cc9b5282c1bd47023d51
Xau Gia Hung <hungbanana659@gmail.com>
2024-11-20 15:51:17 +0700

    done

--- Poss System/FrmMain.cs
+++ Poss System/FrmMain.cs
@@ -19,17 +19,17 @@ namespace Poss_System
 
         private void button3_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+          
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-          
+            
         }
     }
 }
