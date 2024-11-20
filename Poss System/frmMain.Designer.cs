f697ada92960bf1d78e635e3454ba40efa7f6b8b
Khang Nguyễn <kn054734@gmail.com>
2024-11-21 00:10:43 +0700

    hehe

--- Poss System/frmMain.Designer.cs
+++ Poss System/frmMain.Designer.cs
@@ -29,12 +29,6 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.BtnExit = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
@@ -43,10 +37,17 @@
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button11 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -64,78 +65,10 @@
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(306, 754);
+            this.panel1.Size = new System.Drawing.Size(230, 613);
             this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Controls.Add(this.button3);
-            this.panel2.Controls.Add(this.BtnExit);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(306, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(957, 55);
-            this.panel2.TabIndex = 1;
-            // 
-            // panel3
-            // 
-            this.panel3.Location = new System.Drawing.Point(368, 132);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(114, 82);
-            this.panel3.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(47, 132);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(208, 39);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Quản Lý Nhà Hàng";
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.Transparent;
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.Image = global::Poss_System.Properties.Resources.minus;
-            this.button2.Location = new System.Drawing.Point(777, 8);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(50, 43);
-            this.button2.TabIndex = 0;
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.BtnExit_Click);
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.Transparent;
-            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button3.Image = global::Poss_System.Properties.Resources.maximize;
-            this.button3.Location = new System.Drawing.Point(833, 7);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(53, 45);
-            this.button3.TabIndex = 0;
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.BtnExit_Click);
-            // 
-            // BtnExit
-            // 
-            this.BtnExit.BackColor = System.Drawing.Color.Transparent;
-            this.BtnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.BtnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.BtnExit.Image = global::Poss_System.Properties.Resources.close_window__1_;
-            this.BtnExit.Location = new System.Drawing.Point(892, 7);
-            this.BtnExit.Name = "BtnExit";
-            this.BtnExit.Size = new System.Drawing.Size(53, 43);
-            this.BtnExit.TabIndex = 0;
-            this.BtnExit.UseVisualStyleBackColor = false;
-            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
             // 
             // button10
             // 
@@ -146,9 +79,10 @@
             this.button10.ForeColor = System.Drawing.Color.White;
             this.button10.Image = global::Poss_System.Properties.Resources.setting;
             this.button10.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button10.Location = new System.Drawing.Point(71, 650);
+            this.button10.Location = new System.Drawing.Point(53, 528);
+            this.button10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(169, 61);
+            this.button10.Size = new System.Drawing.Size(127, 50);
             this.button10.TabIndex = 2;
             this.button10.Text = "Settings";
             this.button10.UseVisualStyleBackColor = false;
@@ -162,9 +96,10 @@
             this.button9.ForeColor = System.Drawing.Color.White;
             this.button9.Image = global::Poss_System.Properties.Resources.cooking;
             this.button9.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button9.Location = new System.Drawing.Point(71, 583);
+            this.button9.Location = new System.Drawing.Point(53, 474);
+            this.button9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(169, 61);
+            this.button9.Size = new System.Drawing.Size(127, 50);
             this.button9.TabIndex = 2;
             this.button9.Text = "Kitchen";
             this.button9.UseVisualStyleBackColor = false;
@@ -178,9 +113,10 @@
             this.button8.ForeColor = System.Drawing.Color.White;
             this.button8.Image = global::Poss_System.Properties.Resources.pos_terminal;
             this.button8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button8.Location = new System.Drawing.Point(71, 516);
+            this.button8.Location = new System.Drawing.Point(53, 419);
+            this.button8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(169, 61);
+            this.button8.Size = new System.Drawing.Size(127, 50);
             this.button8.TabIndex = 2;
             this.button8.Text = "POS";
             this.button8.UseVisualStyleBackColor = false;
@@ -194,9 +130,10 @@
             this.button7.ForeColor = System.Drawing.Color.White;
             this.button7.Image = global::Poss_System.Properties.Resources.steward;
             this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button7.Location = new System.Drawing.Point(71, 449);
+            this.button7.Location = new System.Drawing.Point(53, 365);
+            this.button7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(169, 61);
+            this.button7.Size = new System.Drawing.Size(127, 50);
             this.button7.TabIndex = 2;
             this.button7.Text = "Staffs";
             this.button7.UseVisualStyleBackColor = false;
@@ -210,9 +147,10 @@
             this.button6.ForeColor = System.Drawing.Color.White;
             this.button6.Image = global::Poss_System.Properties.Resources.round_table;
             this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button6.Location = new System.Drawing.Point(71, 382);
+            this.button6.Location = new System.Drawing.Point(53, 310);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(169, 61);
+            this.button6.Size = new System.Drawing.Size(127, 50);
             this.button6.TabIndex = 2;
             this.button6.Text = "Tables";
             this.button6.UseVisualStyleBackColor = false;
@@ -226,9 +164,10 @@
             this.button5.ForeColor = System.Drawing.Color.White;
             this.button5.Image = global::Poss_System.Properties.Resources.box;
             this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button5.Location = new System.Drawing.Point(71, 315);
+            this.button5.Location = new System.Drawing.Point(53, 256);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(169, 61);
+            this.button5.Size = new System.Drawing.Size(127, 50);
             this.button5.TabIndex = 2;
             this.button5.Text = "Products";
             this.button5.UseVisualStyleBackColor = false;
@@ -242,9 +181,10 @@
             this.button4.ForeColor = System.Drawing.Color.White;
             this.button4.Image = global::Poss_System.Properties.Resources.menu;
             this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button4.Location = new System.Drawing.Point(71, 248);
+            this.button4.Location = new System.Drawing.Point(53, 202);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(169, 61);
+            this.button4.Size = new System.Drawing.Size(127, 50);
             this.button4.TabIndex = 2;
             this.button4.Text = "Categories";
             this.button4.UseVisualStyleBackColor = false;
@@ -258,40 +198,128 @@
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Image = global::Poss_System.Properties.Resources.home;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(71, 181);
+            this.button1.Location = new System.Drawing.Point(53, 147);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(169, 61);
+            this.button1.Size = new System.Drawing.Size(127, 50);
             this.button1.TabIndex = 2;
             this.button1.Text = "Home";
             this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(35, 107);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Quản Lý Nhà Hàng";
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox1.Image = global::Poss_System.Properties.Resources._755_food_removebg_preview;
-            this.pictureBox1.Location = new System.Drawing.Point(99, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(74, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(98, 95);
+            this.pictureBox1.Size = new System.Drawing.Size(74, 77);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.BtnExit);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(230, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(717, 45);
+            this.panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Image = global::Poss_System.Properties.Resources.minus;
+            this.button2.Location = new System.Drawing.Point(583, 6);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(38, 35);
+            this.button2.TabIndex = 0;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Image = global::Poss_System.Properties.Resources.maximize;
+            this.button3.Location = new System.Drawing.Point(625, 6);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 37);
+            this.button3.TabIndex = 0;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.BackColor = System.Drawing.Color.Transparent;
+            this.BtnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.BtnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BtnExit.Image = global::Poss_System.Properties.Resources.close_window__1_;
+            this.BtnExit.Location = new System.Drawing.Point(669, 6);
+            this.BtnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(40, 35);
+            this.BtnExit.TabIndex = 0;
+            this.BtnExit.UseVisualStyleBackColor = false;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(276, 107);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(86, 67);
+            this.panel3.TabIndex = 1;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(525, 215);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 2;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
             // FrmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1263, 754);
+            this.ClientSize = new System.Drawing.Size(947, 613);
+            this.Controls.Add(this.button11);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FrmMain";
             this.Text = "frmMain";
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -314,5 +342,6 @@
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button11;
     }
 }
