42f829bf1f0f8587971ab6ae97db72afec3f9a13
Phúc Trương <hoangphuc30082004@gmail.com>
2024-11-23 00:39:18 +0700

    aa

--- Poss System/frmMain.Designer.cs
+++ Poss System/frmMain.Designer.cs
@@ -29,54 +29,312 @@
         private void InitializeComponent()
         {
             this.panel2 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureIconMenu = new System.Windows.Forms.PictureBox();
+            this.lblNameRes = new System.Windows.Forms.Label();
+            this.pictureIconBurger = new System.Windows.Forms.PictureBox();
+            this.menupanel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.table3 = new Poss_System.Component.Table();
+            this.table2 = new Poss_System.Component.Table();
+            this.table1 = new Poss_System.Component.Table();
+            this.table4 = new Poss_System.Component.Table();
+            this.table5 = new Poss_System.Component.Table();
+            this.table6 = new Poss_System.Component.Table();
+            this.table7 = new Poss_System.Component.Table();
+            this.table8 = new Poss_System.Component.Table();
+            this.table9 = new Poss_System.Component.Table();
+            this.table10 = new Poss_System.Component.Table();
+            this.table11 = new Poss_System.Component.Table();
+            this.table12 = new Poss_System.Component.Table();
+            this.table13 = new Poss_System.Component.Table();
+            this.table14 = new Poss_System.Component.Table();
+            this.table15 = new Poss_System.Component.Table();
+            this.table16 = new Poss_System.Component.Table();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureIconMenu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureIconBurger)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel2
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(119)))), ((int)(((byte)(38)))));
-            this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(135)))), ((int)(((byte)(43)))));
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.pictureIconMenu);
+            this.panel2.Controls.Add(this.lblNameRes);
+            this.panel2.Controls.Add(this.pictureIconBurger);
+            this.panel2.Location = new System.Drawing.Point(12, 11);
             this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1263, 75);
+            this.panel2.Size = new System.Drawing.Size(1328, 58);
             this.panel2.TabIndex = 1;
             // 
-            // pictureBox1
+            // pictureIconMenu
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox1.Image = global::Poss_System.Properties.Resources._755_food_removebg_preview;
-            this.pictureBox1.Location = new System.Drawing.Point(1176, -2);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(87, 79);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.pictureIconMenu.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.pictureIconMenu.BackColor = System.Drawing.Color.Transparent;
+            this.pictureIconMenu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureIconMenu.Image = global::Poss_System.Properties.Resources.list1;
+            this.pictureIconMenu.Location = new System.Drawing.Point(1260, 11);
+            this.pictureIconMenu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureIconMenu.Name = "pictureIconMenu";
+            this.pictureIconMenu.Size = new System.Drawing.Size(63, 32);
+            this.pictureIconMenu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureIconMenu.TabIndex = 4;
+            this.pictureIconMenu.TabStop = false;
+            // 
+            // lblNameRes
+            // 
+            this.lblNameRes.AutoSize = true;
+            this.lblNameRes.BackColor = System.Drawing.Color.Transparent;
+            this.lblNameRes.Font = new System.Drawing.Font("Monotype Corsiva", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNameRes.ForeColor = System.Drawing.Color.White;
+            this.lblNameRes.Location = new System.Drawing.Point(85, 12);
+            this.lblNameRes.Name = "lblNameRes";
+            this.lblNameRes.Size = new System.Drawing.Size(282, 34);
+            this.lblNameRes.TabIndex = 3;
+            this.lblNameRes.Text = "Tam Thai Tu Restaurant";
+            // 
+            // pictureIconBurger
+            // 
+            this.pictureIconBurger.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.pictureIconBurger.BackColor = System.Drawing.Color.Transparent;
+            this.pictureIconBurger.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureIconBurger.Image = global::Poss_System.Properties.Resources.burger;
+            this.pictureIconBurger.Location = new System.Drawing.Point(16, 11);
+            this.pictureIconBurger.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureIconBurger.Name = "pictureIconBurger";
+            this.pictureIconBurger.Size = new System.Drawing.Size(63, 32);
+            this.pictureIconBurger.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureIconBurger.TabIndex = 2;
+            this.pictureIconBurger.TabStop = false;
+            // 
+            // menupanel
+            // 
+            this.menupanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.menupanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(135)))), ((int)(((byte)(43)))));
+            this.menupanel.Location = new System.Drawing.Point(826, 60);
+            this.menupanel.Name = "menupanel";
+            this.menupanel.Size = new System.Drawing.Size(514, 749);
+            this.menupanel.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.table13);
+            this.panel1.Controls.Add(this.table14);
+            this.panel1.Controls.Add(this.table15);
+            this.panel1.Controls.Add(this.table16);
+            this.panel1.Controls.Add(this.table9);
+            this.panel1.Controls.Add(this.table10);
+            this.panel1.Controls.Add(this.table11);
+            this.panel1.Controls.Add(this.table12);
+            this.panel1.Controls.Add(this.table5);
+            this.panel1.Controls.Add(this.table6);
+            this.panel1.Controls.Add(this.table7);
+            this.panel1.Controls.Add(this.table8);
+            this.panel1.Controls.Add(this.table4);
+            this.panel1.Controls.Add(this.table3);
+            this.panel1.Controls.Add(this.table2);
+            this.panel1.Controls.Add(this.table1);
+            this.panel1.Location = new System.Drawing.Point(12, 74);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1328, 720);
+            this.panel1.TabIndex = 3;
+            // 
+            // table3
+            // 
+            this.table3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table3.Location = new System.Drawing.Point(657, 19);
+            this.table3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table3.Name = "table3";
+            this.table3.Size = new System.Drawing.Size(271, 152);
+            this.table3.TabIndex = 2;
+            // 
+            // table2
+            // 
+            this.table2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table2.Location = new System.Drawing.Point(341, 19);
+            this.table2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table2.Name = "table2";
+            this.table2.Size = new System.Drawing.Size(271, 152);
+            this.table2.TabIndex = 1;
+            // 
+            // table1
+            // 
+            this.table1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table1.Location = new System.Drawing.Point(27, 19);
+            this.table1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table1.Name = "table1";
+            this.table1.Size = new System.Drawing.Size(271, 152);
+            this.table1.TabIndex = 0;
+            // 
+            // table4
+            // 
+            this.table4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table4.Location = new System.Drawing.Point(1010, 19);
+            this.table4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table4.Name = "table4";
+            this.table4.Size = new System.Drawing.Size(271, 152);
+            this.table4.TabIndex = 3;
+            // 
+            // table5
+            // 
+            this.table5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table5.Location = new System.Drawing.Point(1010, 201);
+            this.table5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table5.Name = "table5";
+            this.table5.Size = new System.Drawing.Size(271, 152);
+            this.table5.TabIndex = 7;
+            // 
+            // table6
+            // 
+            this.table6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table6.Location = new System.Drawing.Point(657, 201);
+            this.table6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table6.Name = "table6";
+            this.table6.Size = new System.Drawing.Size(271, 152);
+            this.table6.TabIndex = 6;
+            // 
+            // table7
+            // 
+            this.table7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table7.Location = new System.Drawing.Point(341, 201);
+            this.table7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table7.Name = "table7";
+            this.table7.Size = new System.Drawing.Size(271, 152);
+            this.table7.TabIndex = 5;
+            // 
+            // table8
+            // 
+            this.table8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table8.Location = new System.Drawing.Point(27, 201);
+            this.table8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table8.Name = "table8";
+            this.table8.Size = new System.Drawing.Size(271, 152);
+            this.table8.TabIndex = 4;
+            // 
+            // table9
+            // 
+            this.table9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table9.Location = new System.Drawing.Point(1010, 384);
+            this.table9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table9.Name = "table9";
+            this.table9.Size = new System.Drawing.Size(271, 152);
+            this.table9.TabIndex = 11;
+            // 
+            // table10
+            // 
+            this.table10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table10.Location = new System.Drawing.Point(657, 384);
+            this.table10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table10.Name = "table10";
+            this.table10.Size = new System.Drawing.Size(271, 152);
+            this.table10.TabIndex = 10;
+            // 
+            // table11
+            // 
+            this.table11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table11.Location = new System.Drawing.Point(341, 384);
+            this.table11.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table11.Name = "table11";
+            this.table11.Size = new System.Drawing.Size(271, 152);
+            this.table11.TabIndex = 9;
+            // 
+            // table12
+            // 
+            this.table12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table12.Location = new System.Drawing.Point(27, 384);
+            this.table12.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table12.Name = "table12";
+            this.table12.Size = new System.Drawing.Size(271, 152);
+            this.table12.TabIndex = 8;
+            // 
+            // table13
+            // 
+            this.table13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table13.Location = new System.Drawing.Point(1010, 568);
+            this.table13.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table13.Name = "table13";
+            this.table13.Size = new System.Drawing.Size(271, 152);
+            this.table13.TabIndex = 15;
+            // 
+            // table14
+            // 
+            this.table14.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table14.Location = new System.Drawing.Point(657, 568);
+            this.table14.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table14.Name = "table14";
+            this.table14.Size = new System.Drawing.Size(271, 152);
+            this.table14.TabIndex = 14;
+            // 
+            // table15
+            // 
+            this.table15.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table15.Location = new System.Drawing.Point(341, 568);
+            this.table15.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table15.Name = "table15";
+            this.table15.Size = new System.Drawing.Size(271, 152);
+            this.table15.TabIndex = 13;
+            // 
+            // table16
+            // 
+            this.table16.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.table16.Location = new System.Drawing.Point(27, 568);
+            this.table16.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table16.Name = "table16";
+            this.table16.Size = new System.Drawing.Size(271, 152);
+            this.table16.TabIndex = 12;
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1263, 754);
+            this.ClientSize = new System.Drawing.Size(1352, 795);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menupanel);
             this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FrmMain";
             this.Text = "frmMain";
             this.panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureIconMenu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureIconBurger)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureIconBurger;
+        private System.Windows.Forms.Label lblNameRes;
+        private System.Windows.Forms.PictureBox pictureIconMenu;
+        private System.Windows.Forms.Panel menupanel;
+        private System.Windows.Forms.Panel panel1;
+        private Component.Table table3;
+        private Component.Table table2;
+        private Component.Table table1;
+        private Component.Table table13;
+        private Component.Table table14;
+        private Component.Table table15;
+        private Component.Table table16;
+        private Component.Table table9;
+        private Component.Table table10;
+        private Component.Table table11;
+        private Component.Table table12;
+        private Component.Table table5;
+        private Component.Table table6;
+        private Component.Table table7;
+        private Component.Table table8;
+        private Component.Table table4;
     }
 }
